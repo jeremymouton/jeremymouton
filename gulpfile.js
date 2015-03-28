2d977c005375853772553fb4d561ab58b72ce901
Jeremy Mouton <jeremy@aspendigital.com>
2015-03-28 10:47:41 -0600

    cleanup old setup, add gulp and bower back

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+// ======================
+// GULPFILE
+// ======================
+
+// Load plugins
+var
+  gulp         = require('gulp'),
+  less         = require('gulp-less'),
+  bless        = require('gulp-bless'),
+  minifycss    = require('gulp-minify-css'),
+  uglify       = require('gulp-uglify'),
+  rimraf       = require('gulp-rimraf'),
+  concat       = require('gulp-concat'),
+  notify       = require('gulp-notify'),
+  rename       = require('gulp-rename'),
+  path         = require('path'),
+  autoprefixer = require('gulp-autoprefixer'),
+  livereload   = require('gulp-livereload');
+
+// CSS
+gulp.task('css', function() {
+  var stream = gulp
+    .src('src/less/styles.less')
+    .pipe(less().on('error', notify.onError(function (error) {
+      return 'Error compiling LESS: ' + error.message;
+    })))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('public/css'));
+
+  return stream
+    .pipe(minifycss())
+    .pipe(rename(function (path) {
+      if(path.extname === '.css') {
+        path.basename += '.min';
+      }
+    }))
+    .pipe(bless())
+    .pipe(gulp.dest('public/css'))
+    .pipe(notify({ message: 'Successfully compiled LESS' }));
+});
+
+// JS
+gulp.task('js', function() {
+  var scripts = [
+    'src/components/jquery/dist/jquery.js',
+    'src/components/momentjs/moment.js',
+    'src/js/main.js'
+  ];
+
+  var stream = gulp
+    .src(scripts)
+    .pipe(concat('script.js'));
+
+  return stream
+    .pipe(gulp.dest('public/js'))
+    .pipe(uglify({ outSourceMap: true }))
+    .pipe(rename(function (path) {
+      if(path.extname === '.js') {
+        path.basename += '.min';
+      }
+    }))
+    .pipe(gulp.dest('public/js'))
+    .pipe(notify({ message: 'Successfully compiled JavaScript' }));
+});
+
+// Rimraf
+gulp.task('rimraf', function() {
+  return gulp
+    .src(['public/css', 'public/js'], {read: false})
+    .pipe(rimraf());
+});
+
+// Default task
+gulp.task('default', ['rimraf'], function() {
+  gulp.start('css', 'js');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .less files
+  gulp.watch('src/less/**/*.less', ['css']);
+
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['js']);
+
+  // Livereload
+  livereload.listen();
+  gulp.watch('public/**/*').on('change', livereload.changed);
+  
+});
