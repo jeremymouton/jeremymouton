5ceb62ce2c78870525443f9b5b93d96477da98fa
Jeremy Mouton <jeremy@aspendigital.com>
2017-02-23 21:11:25 -0700

    Kill diamond

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="section">
     <latest-track></latest-track>
-    <span class="◊">◊</span>
   </section>
 </template>
 
@@ -15,39 +14,3 @@ export default {
   },
 }
 </script>
-
-<style lang="less">
-.◊ {
-  display: none;
-
-  @media (min-height: 460px) {
-    text-align: center;
-    display: inline-block;
-    position: absolute;
-    left: 50%;
-    opacity: .1;
-    transform: translateY(-50%);
-    bottom: 10vh;
-    transition: all .12s ease;
-    animation: hover 2s ease-in infinite,
-               glow 6s ease-in infinite;
-  }
-
-  &:hover {
-    cursor: crosshair;
-  }
-}
-
-@keyframes glow {
-  0% { opacity: .1; }
-  70% { opacity: .3; }
-  100% { opacity: .1; }
-}
-
-@keyframes hover {
-  0% { transform: translate3d(0, 0, 0); }
-  50% { transform: translate3d(0, 4px, 0); }
-  70% { transform: translate3d(0, 2px, 0); }
-  100% { transform: translate3d(0, 0, 0); }
-}
-</style>
